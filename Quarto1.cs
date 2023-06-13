a4834c56128922e8a9e849e321d1fe7f3974dd1d
BrenoCSenna <100354416+BrenoCSenna@users.noreply.github.com>
2023-06-13 00:01:09 -0300

    Update Quarto1.cs

--- Quarto1.cs
+++ Quarto1.cs
@@ -1,1 +1,34 @@
+using ExercicioAluguelQuarto;
+using System;
+using System.Globalization;
 
+namespace AlugueldeQuarto {
+    class Project {
+        static void Main(string[] args) {
+            
+            Console.Write("How many room will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Booking[] vect = new Booking[10];
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Rent #"+i);
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int numero = int.Parse(Console.ReadLine());
+                vect[i] = new Booking (name,email);
+            }
+            Console.WriteLine("Busy Rooms: ");
+            for (int i = 0; i <= n; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i +": " + vect[i]);
+                }
+
+            }
+
+        }
+    }
+}
